7a073c01ddbb59920d7fad75f85d382b1e0419b5
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-05-23 23:01:14 -0500

    Update Files

--- managementServer.py
+++ managementServer.py
@@ -214,7 +214,7 @@ def makeVideo():
             frames.append( './managementServer/processingServer0' + str( a ) + '/' + str( numframes ) + '.jpg' )
     frame = cv2.imread( './managementServer/processingServer01/1.jpg' )
     fourcc = cv2.VideoWriter_fourcc( *'mp4v' )
-    video = cv2.VideoWriter( dirVideo + 'processedvideo.mp4' , fourcc , 25.0 , ( frame.shape[1] , frame.shape[0] ) )
+    video = cv2.VideoWriter( dirVideo + 'processedvideo.mp4' , fourcc , 60.0 , ( frame.shape[1] , frame.shape[0] ) )
     for i in frames:
         video.write( cv2.imread( i ) )
     cv2.destroyAllWindows()
